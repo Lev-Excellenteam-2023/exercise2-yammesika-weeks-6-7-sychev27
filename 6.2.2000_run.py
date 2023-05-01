bb186c8d33654fb6621e9f84beb5491d48c5df8c
alex <alexsychev27@gmail.com>
2023-05-01 16:53:38 +0300

    fixed the code as requested in pull request

--- 6.2.2000_run.py
+++ 6.2.2000_run.py
@@ -1,14 +1,15 @@
 import time
 
 
-def timer(f, *args, **kwargs):
-    st = time.time()
-    n = list(f(*args, **kwargs))
-    et = time.time()
-    elapsed_time = et - st
+def timer(function, *args, **kwargs):
+    start_time = time.time()
+    my_list = list(function(*args, **kwargs))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
     return elapsed_time
 
 
-print(timer(zip, [1, 2, 3], [4, 5, 6]))
+if __name__ == "__main__":
+    print(timer(zip, [1, 2, 3], [4, 5, 6]))
 
 
